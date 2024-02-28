f310adec3c355294de1e918eba9e365f2a8f2393
mattpe <mattpe@metropolia.fi>
2024-02-28 14:55:48 +0200

    add simple video player

--- src/views/Single.tsx
+++ src/views/Single.tsx
@@ -1,16 +1,30 @@
 import {Image, Text, View} from 'react-native';
+import {Video, ResizeMode} from 'expo-av';
 import {MediaItemWithOwner} from '../types/DBTypes';
 
-const Single = ({route}: {route: {params: MediaItemWithOwner}}) => {
-  const item = route.params;
+const Single = ({route}: any) => {
+  const item: MediaItemWithOwner = route.params;
+  const [fileType, fileFormat] = item.media_type.split('&#x2F;');
+
   return (
     <View>
       <Text>{item.title}</Text>
-      <Image style={{height: 500}} source={{uri: 'http:' + item.filename}} />
+      {fileType === 'image' ? (
+        <Image style={{height: 500}} source={{uri: 'http:' + item.filename}} />
+      ) : (
+        <Video
+          style={{height: 400}}
+          source={{uri: 'http:' + item.filename}}
+          useNativeControls
+          resizeMode={ResizeMode.CONTAIN}
+        />
+      )}
       <Text>{item.description}</Text>
       <Text>{new Date(item.created_at).toLocaleString('fi-FI')}</Text>
       <Text>Owner: {item.username}</Text>
-      <Text>Media type: {item.media_type.split('&#x2F;')[0]}</Text>
+      <Text>
+        Media type: {fileType} / {fileFormat}
+      </Text>
       <Text>File size: {Math.round(item.filesize / 1024)} kB</Text>
     </View>
   );
