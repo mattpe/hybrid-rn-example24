21f44d08b60a7d9f2a41d8a0ff1bb6cff8f96102
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2024-02-29 14:10:39 +0200

    login started

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,10 +1,66 @@
-import {View, Text} from 'react-native';
+import {Controller, useForm} from 'react-hook-form';
+import {Button, Card, Input} from '@rneui/base';
+import {useUserContext} from '../hooks/ContextHooks';
+import {Credentials} from '../types/LocalTypes';
 
 const LoginForm = () => {
+  const {handleLogin} = useUserContext();
+  const initValues: Credentials = {username: '', password: ''};
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm({
+    defaultValues: initValues,
+  });
+
+  const doLogin = async (inputs: Credentials) => {
+    handleLogin(inputs);
+  };
+
   return (
-    <View>
-      <Text>LoginForm</Text>
-    </View>
+    <Card>
+      <Controller
+        control={control}
+        rules={{
+          required: {
+            value: true,
+            message: 'Käyttäjänimi vaaditaan vitun pelle',
+          },
+        }}
+        render={({field: {onChange, onBlur, value}}) => (
+          <Input
+            placeholder="Username"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            autoCapitalize="none"
+            errorMessage={errors.username?.message}
+          />
+        )}
+        name="username"
+      />
+
+      <Controller
+        control={control}
+        rules={{
+          maxLength: 100,
+          required: {value: true, message: 'is required'},
+        }}
+        render={({field: {onChange, onBlur, value}}) => (
+          <Input
+            placeholder="password"
+            secureTextEntry
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            errorMessage={errors.password?.message}
+          />
+        )}
+        name="password"
+      />
+      <Button title="Login" onPress={handleSubmit(doLogin)} />
+    </Card>
   );
 };
 
--- src/navigators/Navigator.tsx
+++ src/navigators/Navigator.tsx
@@ -4,6 +4,8 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from '../views/Home';
 import Profile from '../views/Profile';
 import Single from '../views/Single';
+import Login from '../views/Login';
+import {useUserContext} from '../hooks/ContextHooks';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -18,14 +20,22 @@ const TabNavigator = () => {
 };
 
 const StackNavigator = () => {
+  const {user} = useUserContext();
+
   return (
     <Stack.Navigator>
-      <Stack.Screen
-        name="Tabs"
-        component={TabNavigator}
-        options={{headerShown: false}}
-      />
-      <Stack.Screen name="Single Media" component={Single} />
+      {user ? (
+        <>
+          <Stack.Screen
+            name="Tabs"
+            component={TabNavigator}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen name="Single Media" component={Single} />
+        </>
+      ) : (
+        <Stack.Screen name="Login" component={Login} />
+      )}
     </Stack.Navigator>
   );
 };
